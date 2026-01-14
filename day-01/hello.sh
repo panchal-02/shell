c9e813dcacbc9e45d689c67a33d514b1e6527fe8
Super User <root@ip-172-31-11-98.ap-south-1.compute.internal>
2026-01-14 13:32:05 +0000

    day-01

---
+++ day-01/hello.sh
@@ -0,0 +1,34 @@
+#this is my day01 of shell
+
+echo " enter your name: "  
+read name
+
+     sleep 2
+ echo " wlecome $name "
+     sleep 2
+
+ echo " how r u today "
+    sleep 2
+
+ read xy
+    sleep 2
+ echo " nice to hear that u r $xy "
+    sleep 2
+ 
+ echo " what u want to do today "
+ read x
+ sleep 2
+ echo " sound intresting $x "
+    sleep 2
+   echo " .
+   .
+  .
+ .
+.
+.
+.
+.
+"
+sleep 3
+
+    echo " end "
